065a54b105c1f46ed2fb2454fab036b0719a96dd
AmicBeam <1425257712@qq.com>
2026-03-29 23:40:25 +0800

    feat: 属性标签
    Change-Id: Ie418ca05fe7dafb949c95a99a49b6909fd1a7193

--- app.py
+++ app.py
@@ -8,23 +8,23 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.jinja_env.auto_reload = True
 
 MAIN_CANDIDATES = [
-    {"id": "bohe", "name": "薄荷", "image": "薄荷.png"},
-    {"id": "yi", "name": "翳", "image": "翳.png"},
-    {"id": "baicang", "name": "白藏", "image": "白藏.png"},
-    {"id": "nanali", "name": "娜娜莉", "image": "娜娜莉.png"},
-    {"id": "xiaozhi", "name": "小吱", "image": "小吱.png"},
+    {"id": "bohe", "name": "薄荷", "image": "薄荷.png", "elem": "灵"},
+    {"id": "yi", "name": "翳", "image": "翳.png", "elem": "相"},
+    {"id": "baicang", "name": "白藏", "image": "白藏.png", "elem": "咒"},
+    {"id": "nanali", "name": "娜娜莉", "image": "娜娜莉.png", "elem": "灵"},
+    {"id": "xiaozhi", "name": "小吱", "image": "小吱.png", "elem": "光"},
 ]
 
 TEAMMATES = [
-    {"id": "zhujue", "name": "主角", "image": "男主.png"},
-    {"id": "aidejia", "name": "埃德嘉", "image": "埃德嘉.png"},
-    {"id": "jiuyuan", "name": "九原", "image": "九原.png"},
-    {"id": "zaowu", "name": "早雾", "image": "早雾.png"},
-    {"id": "adele", "name": "阿德勒", "image": "阿德勒.png"},
-    {"id": "dafutier", "name": "达芙蒂尔（最好1觉）", "image": "达芙蒂尔.png"},
-    {"id": "fatiya", "name": "法蒂娅", "image": "法帝娅.png"},
-    {"id": "haniya", "name": "哈尼娅", "image": "哈尼娅.png"},
-    {"id": "hasuoer", "name": "哈索尔", "image": "哈索尔.png"},
+    {"id": "zhujue", "name": "主角", "image": "男主.png", "elem": "光"},
+    {"id": "aidejia", "name": "埃德嘉", "image": "埃德嘉.png", "elem": "光"},
+    {"id": "jiuyuan", "name": "九原", "image": "九原.png", "elem": "灵"},
+    {"id": "zaowu", "name": "早雾", "image": "早雾.png", "elem": "咒"},
+    {"id": "adele", "name": "阿德勒", "image": "阿德勒.png", "elem": "咒"},
+    {"id": "dafutier", "name": "达芙蒂尔（最好1觉）", "image": "达芙蒂尔.png", "elem": "暗"},
+    {"id": "fatiya", "name": "法蒂娅", "image": "法帝娅.png", "elem": "魂"},
+    {"id": "haniya", "name": "哈尼娅", "image": "哈尼娅.png", "elem": "魂"},
+    {"id": "hasuoer", "name": "哈索尔", "image": "哈索尔.png", "elem": "相"},
 ]
 
 @app.route("/")
