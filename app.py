fa796553c0123f957bf74219c40158ebefe61f89
AmicBeam <1425257712@qq.com>
2026-03-30 16:17:29 +0800

    feat: 队友位安魂曲
    Change-Id: I4e7a12c91201b8a480a147c90fffc2c0a73f63b1

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ MAIN_CANDIDATES = [
     {"id": "baicang", "name": "白藏", "image": "白藏.png", "elem": "咒"},
     {"id": "nanali", "name": "娜娜莉", "image": "娜娜莉.png", "elem": "灵"},
     {"id": "xiaozhi", "name": "小吱", "image": "小吱.png", "elem": "光"},
-    {"id": "anhunqu", "name": "安魂曲", "image": "安魂曲.png", "elem": "暗"},
+    {"id": "anhunqu0", "name": "安魂曲（0觉）", "image": "安魂曲.png", "elem": "暗"},
+    {"id": "anhunqu1", "name": "安魂曲（1觉）", "image": "安魂曲.png", "elem": "暗"},
 ]
 
 TEAMMATES = [
@@ -23,6 +24,7 @@ TEAMMATES = [
     {"id": "jiuyuan", "name": "九原", "image": "九原.png", "elem": "灵"},
     {"id": "zaowu", "name": "早雾", "image": "早雾.png", "elem": "咒"},
     {"id": "adele", "name": "阿德勒", "image": "阿德勒.png", "elem": "咒"},
+    {"id": "anhunquT1", "name": "安魂曲（1觉）", "image": "安魂曲.png", "elem": "暗"},
     {"id": "dafutier0", "name": "达芙蒂尔（0觉）", "image": "达芙蒂尔.png", "elem": "暗"},
     {"id": "dafutier1", "name": "达芙蒂尔（1觉）", "image": "达芙蒂尔.png", "elem": "暗"},
     {"id": "fatiya", "name": "法蒂娅", "image": "法帝娅.png", "elem": "魂"},
