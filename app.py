f2603b75ffac4ac1beaedf8dc33376d7369775ae
AmicBeam <1425257712@qq.com>
2026-03-30 14:59:13 +0800

    feat: 角色更新&攻击稀释&娜娜莉削弱
    Change-Id: I3fa52e326470dedafc6bd6cc18e350d6308d405c

--- app.py
+++ app.py
@@ -13,15 +13,18 @@ MAIN_CANDIDATES = [
     {"id": "baicang", "name": "白藏", "image": "白藏.png", "elem": "咒"},
     {"id": "nanali", "name": "娜娜莉", "image": "娜娜莉.png", "elem": "灵"},
     {"id": "xiaozhi", "name": "小吱", "image": "小吱.png", "elem": "光"},
+    {"id": "anhunqu", "name": "安魂曲", "image": "安魂曲.png", "elem": "暗"},
 ]
 
 TEAMMATES = [
     {"id": "zhujue", "name": "主角", "image": "男主.png", "elem": "光"},
+    {"id": "xun", "name": "浔", "image": "浔.png", "elem": "光"},
     {"id": "aidejia", "name": "埃德嘉", "image": "埃德嘉.png", "elem": "光"},
     {"id": "jiuyuan", "name": "九原", "image": "九原.png", "elem": "灵"},
     {"id": "zaowu", "name": "早雾", "image": "早雾.png", "elem": "咒"},
     {"id": "adele", "name": "阿德勒", "image": "阿德勒.png", "elem": "咒"},
-    {"id": "dafutier", "name": "达芙蒂尔（1觉或弹刀）", "image": "达芙蒂尔.png", "elem": "暗"},
+    {"id": "dafutier0", "name": "达芙蒂尔（0觉）", "image": "达芙蒂尔.png", "elem": "暗"},
+    {"id": "dafutier1", "name": "达芙蒂尔（1觉）", "image": "达芙蒂尔.png", "elem": "暗"},
     {"id": "fatiya", "name": "法蒂娅", "image": "法帝娅.png", "elem": "魂"},
     {"id": "haniya", "name": "哈尼娅", "image": "哈尼娅.png", "elem": "魂"},
     {"id": "hasuoer", "name": "哈索尔", "image": "哈索尔.png", "elem": "相"},
